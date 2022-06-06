97cb8bf12912268b0f1bbff9b3cb4250e0108ca1
gabriell-silva <gabrielsilva.freires2@gmail.com>
2022-06-05 22:28:55 -0300

    Fix: Criando componente e estilo

---
+++ src/components/pages/Company/index.jsx
@@ -0,0 +1,13 @@
+import Container from "../../layouts/Container";
+import Styles from './company.module.css'
+
+export default function Company() {
+    return (
+        <div className={Styles.Container}>
+            <h1>Empresa</h1>
+            <Container customClass="column">
+                    
+            </Container>
+        </div>
+    )
+}
